de4f0b6e423d2436752c695addd3aad0a8e91937
Szelma956 <w.bielinski1@gmail.com>
2020-02-26 09:59:47 +0100

    Dodaj pliki projektów.

---
+++ Matura 2018 Zadanie 4.1.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int litera;
+
+    while (!cin.eof()) {
+
+        cin >> litera;
+    }
+
+}
